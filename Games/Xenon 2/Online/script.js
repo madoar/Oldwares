cbd684940bc9d5d0af240cf4b76f893ac60edca8
Quentin PÂRIS <git@quentin.paris>
2017-01-01 23:54:59 +0100

    Xenon 2

---
+++ Games/Xenon 2/Online/script.js
@@ -0,0 +1,13 @@
+include(["Functions", "QuickScript", "ZipScript"]);
+
+new ZipScript()
+    .name("Prehistorik")
+    .editor("Titus")
+    .applicationHomepage("")
+    .author("Quentin PÂRIS")
+    .url("http://repository.playonlinux.com/divers/oldware/Xenon2.zip")
+    .checksum("9b61e88cad02f663e76fe40a379319a4956546c2")
+    .category("Games")
+    .wineVersion("1.8.6-dos_support")
+    .executable("XENON2.EXE")
+    .go();
