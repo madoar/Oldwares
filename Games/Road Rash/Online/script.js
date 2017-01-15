8665945d6de2e514e78a0e3bf833424448d95409
Quentin PÂRIS <git@quentin.paris>
2017-01-15 16:17:45 +0100

    Road rash

--- Games/Road Rash/Online/script.js
+++ Games/Road Rash/Online/script.js
@@ -8,5 +8,5 @@ new ZipScript()
     .url("http://www.bestoldgames.net/download/bgames/road-rash.zip")
     .checksum("82f99038b86bbd267c64f2d34f30b3209bbe4daa")
     .category("Games")
-    .executable("ROADRASH.EXE")
+    .executable("RASHME.EXE")
     .go();
